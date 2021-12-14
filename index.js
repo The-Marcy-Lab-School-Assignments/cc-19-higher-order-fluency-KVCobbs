f0eb6355dd51c8db8bd71b9a89cfcd5369e188dd
KVCobbs <kenyacobbs9001@gmail.com>
2021-12-14 15:54:41 +0000

    finished

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 //write your code here
-function averageAge (arr) {
+function averageAge (alumni) {
     // reduce and filter
+    //map and then reduce 
     // array of objs is killing 
   /*
+  Idea of what to do
+  For each through the object.
+  Push the obj into an empty arr 
+  
   I'm not sure how to put this in words but here
   It's wrong but the right idea
   let filteredData = data.filter(({ gender }) => gender == 'female'),
     avg = filteredData.reduce((r, c) => r + c.age, 0) / filteredData.length;
   */
+  
+ return Math.floor(alumni.reduce((a, b) => {return a + b.age},0 /alumni.length))
 }
 
-function bananaBread (){
-    
+function bananaBread (alumni){
+  // return Math.floor(alumni.reduce((a, b) => {return a + b.age},0 /alumni.length))
+  return alumni.map((student) => {
+  student.bananaBread = `${student.name} got banana bread at ${student.job}`
+  return student
+    })
 }
 
-function ninetiesBabies () {
-    
+
+
+function ninetiesBabies (alumni) {
+    return alumni.filter(student => student.age > 21)
 }
 
-function addSchool () {
-    
+function addSchool (alumni) {
+    return alumni.map(student => {
+        return (...student, school: 'The Marcy Lab School')
+    })
 }
 
 function allUseJavaScript () {
-    
+    // every 
+    return alumni.every((student) => {
+        student.language === "JavaScript")
 }
 
 function noVoewl (words) {
